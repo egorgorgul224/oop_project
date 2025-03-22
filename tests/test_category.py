3fc7c373679b0ff3d7b8589a4c5780c71cd831f2
egor <scudf24@gmail.com>
2025-03-22 13:36:30 +0300

    add mypy annotation for tests and fixtures, update htmlcov

---
+++ tests/test_category.py
@@ -0,0 +1,24 @@
+from typing import Type
+
+from src.category import Category
+
+
+def test_category_init(smartphone_category: Type[Category], tv_category: Type[Category]) -> None:
+    """Тест проверяет корректное создания экземпляра класса Category с атрибутами name, description, products. Также
+    тест проверяет корректный подсчет количества категорий и количества продуктов в этой категории"""
+    assert smartphone_category.name == "Смартфоны"
+    assert (
+        smartphone_category.description
+        == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций"
+    )
+    assert len(smartphone_category.products) == 3
+
+    assert tv_category.name == "Телевизоры"
+    assert tv_category.description == "Современный телевизор, который позволяет наслаждаться просмотром"
+    assert len(tv_category.products) == 1
+
+    assert smartphone_category.category_count == 2
+    assert tv_category.category_count == 2
+
+    assert smartphone_category.product_count == 4
+    assert tv_category.product_count == 4
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,5 +1,10 @@
+from typing import Type
 
-def test_product_init(samsung_product, iphone_product):
+from src.product import Product
+
+
+def test_product_init(samsung_product: Type[Product], iphone_product: Type[Product]) -> None:
+    """Тест проверяет корректное создания экземпляра класса Product с атрибутами name, description, price, quantity."""
     assert samsung_product.name == "Samsung Galaxy S23 Ultra"
     assert samsung_product.description == "256GB, Серый цвет, 200MP камера"
     assert samsung_product.price == 180000.0
