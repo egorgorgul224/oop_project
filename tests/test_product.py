be610c18452438bc8a56eed60ede64e807c0150c
egor <scudf24@gmail.com>
2025-03-23 16:21:18 +0300

    update htmlcov file, small linter fixes

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,4 +1,4 @@
-from typing import Type
+from typing import Any, Type
 
 from src.product import Product
 
@@ -13,12 +13,14 @@ def test_product_init(samsung_product: Type[Product], iphone_product: Type[Produ
 
 def test_new_product() -> None:
     """Тест проверяет корректное создание нового экземпляра класса Product из словаря данных."""
-    new_product = Product.new_product({
+    new_product = Product.new_product(
+        {
             "name": "Samsung Galaxy S23 Ultra",
             "description": "256GB, Серый цвет, 200MP камера",
             "price": 180000.0,
             "quantity": 5,
-        })
+        }
+    )
     assert new_product.name == "Samsung Galaxy S23 Ultra"
     assert new_product.description == "256GB, Серый цвет, 200MP камера"
     assert new_product.price == 180000.0
@@ -31,7 +33,7 @@ def test_price_setter(samsung_product: Type[Product]) -> None:
     assert samsung_product.price == 100.0
 
 
-def test_price_zero_setter(capsys, samsung_product: Type[Product]) -> None:
+def test_price_zero_setter(capsys: Any, samsung_product: Type[Product]) -> None:
     """Тест проверяет корректный возврат сообщение, если заданная цена меньше или равна 0."""
     samsung_product.price = 0
     captured = capsys.readouterr()
